66b707045c70870edba982b519d64a65e5e2be34
Rohan Karankot <rohankarankot@gmail.com>
2024-08-31 15:04:17 +0530

    auth inited mongo uri connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import { AuthModule } from "./auth/auth.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".env"],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+
+        uri: configService.get<string>("DATABASE_URI"),
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>("JWT_SECRET"),
+          signOptions: {
+            expiresIn: config.get<string | number>("JWT_EXPIRE"),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
